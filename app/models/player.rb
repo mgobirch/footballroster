b464b5d81f4fd8eae1845af93f7ae63c94d7ccf4
mgobirch <birch1bs@gmail.com>
2013-04-03 18:35:36 -0400

    updated eligible years, player position table

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,5 @@
 class Player < ActiveRecord::Base
-	attr_accessible :number, :position, :height, :weight, :year_of_eligibility, :roster_id
+	attr_accessible :number, :position, :height, :weight, :year_of_eligibility, :eligible_years_attributes, :roster_id
 	has_many :eligible_years
 	has_many :rosters, :through => :eligible_years
   accepts_nested_attributes_for :eligible_years
