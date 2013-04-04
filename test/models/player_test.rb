aad3ab06dca342e7971db9f855126b9dc15a79d6
Cory Kaufman-Schofield <cory@corykaufman.com>
2013-04-03 22:47:15 -0400

    Test #1

--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class PlayerTest < ActiveSupport::TestCase
   test "the truth" do
-    assert_equal Player.eligible_years_for_year(2005), [2005, 2006, 2007, 2008]
+    player = Player.new
+    assert_equal player.eligible_years_for_year(2005), [2005, 2006, 2007, 2008]
   end
 end
